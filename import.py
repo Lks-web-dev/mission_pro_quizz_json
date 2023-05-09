0b702781051a98ae037bf974777c9ac660fa400e
Luka Sainsily <luka.sainsily@gmail.com>
2023-05-09 16:33:09 +0200

    Quizz lien HS (-1) Ajout lien quizz +1 Ajout du dossier dans le filename modification key difficulte-> Niveau (pour la compréhension) Ajout d'une key "difficulté" au dictionnaire en lien avec le fichier json d'origine...

--- import.py
+++ import.py
@@ -7,9 +7,10 @@ import unicodedata
 open_quizz_db_data = (
     ("Animaux", "Les chats", "https://www.codeavecjonathan.com/res/mission/openquizzdb_50.json"),
     ("Arts", "Musée du Louvre", "https://www.codeavecjonathan.com/res/mission/openquizzdb_86.json"),
-    ("Bande dessinnée", "Tintin", "https://www.kiwime.com/oqdb/files/2124627384/OpenQuizzDB_124/openquizzdb_124.json"),
+    # ("Bande dessinnée", "Tintin", "https://www.kiwime.com/oqdb/files/2124627384/OpenQuizzDB_124/openquizzdb_124.json"),  # Lien non fonctionnel
     ("Cinéma", "Alien", "https://www.codeavecjonathan.com/res/mission/openquizzdb_241.json"),
     ("Cinéma", "Star wars", "https://www.codeavecjonathan.com/res/mission/openquizzdb_90.json"),
+    ("Vie quotidienne", "Chocolat", "https://www.kiwime.com/oqdb/files/1019394664/OpenQuizzDB_019/openquizzdb_19.json"),  # Ajout + 1 quizz
 )
 
 
@@ -18,7 +19,8 @@ def strip_accents(s):
 
 
 def get_quizz_filename(categorie, titre, difficulte):
-    return strip_accents(categorie).lower().replace(" ", "") + "_" + strip_accents(titre).lower().replace(" ", "") + "_" + strip_accents(difficulte).lower().replace(" ", "") + ".json"
+    # Tous les fichiers json sont désormais dans le dossier quizz_json
+    return "quizz_json/" + strip_accents(categorie).lower().replace(" ", "") + "_" + strip_accents(titre).lower().replace(" ", "") + "_" + strip_accents(difficulte).lower().replace(" ", "") + ".json"
 
 
 def generate_json_file(categorie, titre, url):
@@ -26,19 +28,21 @@ def generate_json_file(categorie, titre, url):
     out_questions_data = []
     response = requests.get(url)
     data = json.loads(response.text)
+    difficulte = data["difficulté"]  # Information sur la difficulté
     all_quizz = data["quizz"]["fr"]
     for quizz_title, quizz_data in all_quizz.items():
         out_filename = get_quizz_filename(categorie, titre, quizz_title)
         print(out_filename)
-        out_questionnaire_data["difficulte"] = quizz_title
+        out_questionnaire_data["niveau"] = quizz_title  # je remplace difficulte par niveau car plus approprié
         for question in quizz_data:
             question_dict = {}
             question_dict["titre"] = question["question"]
             question_dict["choix"] = []
             for ch in question["propositions"]:
-                question_dict["choix"].append((ch, ch==question["réponse"]))
+                question_dict["choix"].append((ch, ch == question["réponse"]))
             out_questions_data.append(question_dict)
         out_questionnaire_data["questions"] = out_questions_data
+        out_questionnaire_data["difficulté"] = difficulte  # Ajouté au dictionnaire
         out_json = json.dumps(out_questionnaire_data)
 
         file = open(out_filename, "w")
